3971f615b08554854909d23cae5e4046fd69f75e
kataomoiyo <114056087+kataomoiyo@users.noreply.github.com>
2022-12-28 19:49:00 +0800

    初コミット

---
+++ src/routes.js
@@ -0,0 +1,36 @@
+const {
+  addBookHandler,
+  getAllBookHandler,
+  getBookDetailByIdHandler,
+  editBookByIdHandler,
+  deleteBookByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBookDetailByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: editBookByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: deleteBookByIdHandler,
+  },
+];
+module.exports = routes;
